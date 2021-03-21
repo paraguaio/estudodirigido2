16b3e6da2d61f4d88c553887ebcbb46c869715bc
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-21 19:51:12 -0300

    Create Desconto.java

---
+++ Desconto.java
@@ -0,0 +1,37 @@
+/*
+ * 
+ */
+package estudodirigido2;
+
+
+public class Desconto 
+{
+    private String nome; 
+    private double valor;
+
+    public String getNome() 
+    {
+        return nome;
+    }
+
+    public void setNome(String nome) 
+    {
+        this.nome = nome;
+    }
+
+    public double getValor() 
+    {
+        return valor;
+    }
+
+    public void setValor(double valor) 
+    {
+        this.valor = valor;
+    }
+     
+    public double calcularDesconto()
+    {
+        return valor * 0.025;
+    }
+    
+}
